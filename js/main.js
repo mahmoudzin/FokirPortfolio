a108bd04aacd259dc00563e08066e825376542c2
Mahmoud shaaban <mahmoud01013193132@gmail.com>
2022-05-18 07:31:17 +0200

    Fixe home & Navbar

--- js/main.js
+++ js/main.js
@@ -3,16 +3,10 @@ let logo = document.querySelector('#logo')
 let icons = document.querySelectorAll('#nav-icons a')
 window.onscroll = function () {
     if(this.scrollY > 20){
-        nav.classList.add('bg-white')
         nav.classList.add('shadow-lg')
-        nav.classList.add('navbar-light')
-        nav.classList.remove('navbar-dark')
-        logo.src = 'images/bakery-color.png'
+        nav.classList.add('bg-dark')
     }else{
-        nav.classList.remove('bg-white')
         nav.classList.remove('shadow-lg')
-        nav.classList.add('navbar-dark')
-        nav.classList.remove('navbar-light')
-        logo.src = 'images/bakery-light-1.png'
+        nav.classList.remove('bg-dark')
     }
 }
